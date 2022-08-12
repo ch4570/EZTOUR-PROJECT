2d195d25ce0fe043a6a1d05f5fb19f1d9f417a0c
Daniel0915 <ufo9363@gmail.com>
2022-08-12 09:41:39 +0900

    [Feature] 리뷰 기능 작업 중 #1

---
+++ src/main/java/com/devcamp/eztour/dao/rvw/RvwDao.java
@@ -0,0 +1,5 @@
+package com.devcamp.eztour.dao.rvw;
+
+public interface RvwDao {
+    int count() throws Exception;
+}
--- src/test/java/com/devcamp/eztour/domain/rvw/PageHandlerTest.java
+++ src/test/java/com/devcamp/eztour/domain/rvw/PageHandlerTest.java
@@ -1,17 +1,22 @@
 package com.devcamp.eztour.domain.rvw;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class PageHandlerTest {
     @Test
     public void test() {
         PageHandler ph = new PageHandler(250, 1);
         ph.print();
         System.out.println("ph = " + ph);
-        assertTrue(ph.beginPage==1);
-        assertTrue(ph.endPage==5);
+        assertTrue(ph.getBeginPage() ==1);
+        assertTrue(ph.getEndPage() ==5);
     }
 
     @Test
@@ -19,8 +24,8 @@ public class PageHandlerTest {
         PageHandler ph = new PageHandler(250, 6);
         ph.print();
         System.out.println("ph = " + ph);
-        assertTrue(ph.beginPage==6);
-        assertTrue(ph.endPage==10);
+        assertTrue(ph.getBeginPage() ==6);
+        assertTrue(ph.getEndPage() ==10);
     }
 
     @Test
@@ -28,8 +33,8 @@ public class PageHandlerTest {
         PageHandler ph = new PageHandler(255, 25);
         ph.print();
         System.out.println("ph = " + ph);
-        assertTrue(ph.beginPage==26);
-        assertTrue(ph.endPage==30);
+        assertTrue(ph.getBeginPage() ==26);
+        assertTrue(ph.getEndPage() ==30);
     }
 
 }
