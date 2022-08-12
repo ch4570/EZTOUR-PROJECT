23040c704f55bf91fbe81eac4d81a6a1a6c72584
JangYeongUk <https://github.com/JYeongUk>
2022-08-12 11:01:15 +0900

    [Design] 레이아웃 구조 설정 #1

--- src/main/java/com/devcamp/eztour/controller/HomeController.java
+++ src/main/java/com/devcamp/eztour/controller/HomeController.java
@@ -1,25 +1,22 @@
 package com.devcamp.eztour.controller;
 
 import com.devcamp.eztour.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    private final TestService testService;
-
-    public HomeController(TestService testService) {
-        this.testService = testService;
-    }
+//    private final TestService testService;
 
     @GetMapping("/")
     public String home() {
 
-
         return "home.tiles";
     }
-
 
 }
