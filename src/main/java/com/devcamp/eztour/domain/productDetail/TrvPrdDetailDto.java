f3210d559ca7668127820d7341481aed9f1513a3
JangYeongUk <https://github.com/JYeongUk>
2022-08-17 15:17:37 +0900

    [Feat] 상품 조회 화면 & 기능 작성 중 #1

--- src/main/java/com/devcamp/eztour/domain/productDetail/TrvPrdDetailDto.java
+++ src/main/java/com/devcamp/eztour/domain/productDetail/TrvPrdDetailDto.java
@@ -5,6 +5,7 @@ import lombok.*;
 import java.util.Date;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode
@@ -12,24 +13,24 @@ public class TrvPrdDetailDto {
 
     private final String prd_cd;
     private final String dstn_cd;
-    private final String cmn_cd_thm;
+    private String cmn_cd_thm;
     private final String prd_nm;
     private final String prd_dtl_desc;
     private final String trv_per;
     private final Integer prd_str_prc;
     private final Date dpr_str_date;
     private final Date dpr_fin_date;
-    private final boolean evnt_yn;
-    private final boolean fin_yn;
-    private final boolean act_yn;
-    private final boolean dc_yn;
-    private final Integer vcnt;
-    private final boolean lk_yn;
+    private boolean evnt_yn;
+    private boolean fin_yn;
+    private boolean act_yn;
+    private boolean dc_yn;
+    private Integer vcnt;
+    private boolean lk_yn;
     private final String img_pth;
-    private final boolean mn_img_yn;
+    private boolean mn_img_yn;
     private final String prd_dtl_cd;
     private final String arl_nm;
-    private final boolean dpr_yn;
-    private final boolean rsvt_yn;
+    private boolean dpr_yn;
+    private boolean rsvt_yn;
 
 }
